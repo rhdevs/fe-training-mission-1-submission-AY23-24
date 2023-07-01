22b5d4046fe0dd2817602f8e07b065dbd044fcd0
LWS49 <111121080+LWS49@users.noreply.github.com>
2023-07-01 21:30:20 +0800

    added submission folder

---
+++ submission/example.ts
@@ -0,0 +1,8 @@
+/**
+ * 1. Clone this repo
+ * 2. Name your submission as <your name>.ts and place it in this submission-folder folder!
+ * 3. Create your own branch, and create a PR to main
+ * 4. Wait for your PR to be approved before you can merge
+ * 5. Once merged, copy the link of your assignment code from this repo and create a card component on the main training repo under Mission1HomePage
+ * 6. Similarly, branch and PR, and merge when ready
+ */
